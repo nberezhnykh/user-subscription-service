65e4f53754004f1d9c61591abca081f817b4f859
Nikita <lokutson@icloud.com>
2025-05-12 17:28:49 +0300

    fix: Add explicit two-argument constructor to TopSubscriptionDto to resolve Hibernate InstantiationException

--- src/main/java/com/github/nberezhnykh/usersubscriptionservice/dto/TopSubscriptionDto.java
+++ src/main/java/com/github/nberezhnykh/usersubscriptionservice/dto/TopSubscriptionDto.java
@@ -1,11 +1,13 @@
 package com.github.nberezhnykh.usersubscriptionservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * Data Transfer Object (DTO) для представления информации о популярной подписке.
  */
 @Data
+@AllArgsConstructor
 public class TopSubscriptionDto {
 
     /**
